5edca2fcbd92e3f2a5ca423dbf0554e48ee44d6d
fortes23 <brais.fortes@gmail.com>
2019-03-27 13:11:54 +0100

    feat: Adapt example to python3

--- examples/example1.py
+++ examples/example1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#Copyright (c) 2013 Miguel Moreto <http://sites.google.com/site/miguelmoreto/>
+# Copyright (c) 2013 Miguel Moreto <http://sites.google.com/site/miguelmoreto/>
 
-#This file is part of pyComtrade.
+# This file is part of pyComtrade.
 #
 #    pyComtrade is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -24,8 +24,8 @@
 # Developed by Miguel Moreto
 # Brazil - 2013
 #
-__version__ = "$Revision$" # SVN revision.
-__date__ = "$Date$" # Date of the last SVN revision.
+__version__ = "$Revision$"  # SVN revision.
+__date__ = "$Date$"         # Date of the last SVN revision.
 
 # Matplotlib module is needed for this example.
 # pyComtrade needs numpy.
@@ -36,12 +36,12 @@ import pylab
 comtradeObj = pyComtrade.ComtradeRecord()
 comtradeObj.read('./test_data3/test3.cfg', './test_data3/test3.dat')
 
-print comtradeObj.get_analog_ids() # print the ids of the analog channels.
+print(comtradeObj.get_analog_ids())  # print the ids of the analog channels.
 
 N = comtradeObj['endsamp'][-1]
 
-print 'Record has %d samples' %(N)
-print 'Sampling rate is %d samples/sec.' %(comtradeObj['samp'][-1])
+print('Record has {} samples'.format(N))
+print('Sampling rate is {} samples/sec.'.format(comtradeObj['samp'][-1]))
 
 # Reading channel 4:
 AnalogChannelData = comtradeObj['A'][22]['values']
@@ -52,14 +52,14 @@ DigitalChannelData = comtradeObj['D'][25]['values']
 time = comtradeObj.get_timestamps()
 
 # Ploting with matplotlib
-#pylab.plot(time,channelData)
+# pylab.plot(time,channelData)
 f, axarr = pylab.subplots(2, sharex=True)
 
 axarr[0].plot(time, AnalogChannelData)
 axarr[0].set_title('pyComtrade Demo')
 axarr[0].grid()
 axarr[1].plot(time, DigitalChannelData)
-axarr[1].set_ylim(top=1.05) # bottom unchanged
+axarr[1].set_ylim(top=1.05)  # bottom unchanged
 axarr[1].grid()
 axarr[1].set_xlabel('Time [s]')
 pylab.show()
