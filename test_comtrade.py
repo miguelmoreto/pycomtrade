f73c4a5ea6c77fd757ef45cf37c6a0cdfb730458
fortes23 <brais.fortes@gmail.com>
2019-03-27 13:17:49 +0100

    test: Check analog ids

--- test_comtrade.py
+++ test_comtrade.py
@@ -14,6 +14,13 @@ class TestPyComtrade(unittest.TestCase):
     def tearDown(self):
         self.comtradeObj.reset()
 
+    def test_analog_ids(self):
+        result = "IA_G1;IB_G1;IC_G1;VA_G1;VB_G1;VC_G1;IA_G2;IB_G2;IC_G2;\
+VA_G2;VB_G2;VC_G2;IA_G3;IB_G3;IC_G3;VA_G3;VB_G3;VC_G3;\
+IA_G4;IB_G4;IC_G4;VA_G4;VB_G4;VC_G4;VA_B69;VB_B69;\
+VC_B69;IN_TF7;IA_TF7;IB_TF7;IC_TF7"
+        self.assertEqual(self.comtradeObj.get_analog_ids(), result)
+
     def test_numsamples(self):
         self.assertEqual(self.comtradeObj['endsamp'][-1], 24768)
 
