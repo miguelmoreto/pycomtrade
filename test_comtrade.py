cc8901e1897d826424ac53a3aebf4c051bf009f3
fortes23 <brais.fortes@gmail.com>
2019-03-27 13:21:34 +0100

    test: Check digital ids

--- test_comtrade.py
+++ test_comtrade.py
@@ -21,6 +21,13 @@ IA_G4;IB_G4;IC_G4;VA_G4;VB_G4;VC_G4;VA_B69;VB_B69;\
 VC_B69;IN_TF7;IA_TF7;IB_TF7;IC_TF7"
         self.assertEqual(self.comtradeObj.get_analog_ids(), result)
 
+    def test_digital_ids(self):
+        result = "86_G1;87UA_G1;87UB_G1;87UC_G1;64S_G1;51_G1;46_G1;32_G1;\
+86_G2;87UA_G2;87UB_G2;87UC_G2;64S_G2;51_G2;46_G2;32_G2;86_G3;\
+87UA_G3;87UB_G3;87UC_G3;64S_G3;51_G3;46_G3;32_G3;86_G4;87UA_G4;\
+87UB_G4;87UC_G4;64S_G4;51_G4;46_G4;32_G4"
+        self.assertEqual(self.comtradeObj.get_digital_ids(), result)
+
     def test_numsamples(self):
         self.assertEqual(self.comtradeObj['endsamp'][-1], 24768)
 
